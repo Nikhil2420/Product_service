099ea48cd6642fd473c2711e5278593345ca5c77
Nikhil <nikhiltiwari98560@gmail.com>
2026-09-20 17:08:07 +0530

    added feature for updating delivery status

--- e-commerce-product-service/src/main/java/com/ProductService/backend/ECommerceProductServiceApplication.java
+++ e-commerce-product-service/src/main/java/com/ProductService/backend/ECommerceProductServiceApplication.java
@@ -2,8 +2,10 @@ package com.ProductService.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ECommerceProductServiceApplication {
 
 	public static void main(String[] args) {
